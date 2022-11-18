d50b8f8db09b18fec2ce948f1f090962d48730dd
Akash Chauhan <akash.sc96@gmail.com>
2022-11-18 06:35:03 -0500

    Complete index.js first draft

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const videoList = require('./routes/video');
+
+app.use('/videos', videoList);
 
 app.listen(PORT, () => {
   console.log("Hello Beautiful!");
