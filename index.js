c58e65f23f7a2da12ca65258b8be7ce9f28d3668
Akash Chauhan <akash.sc96@gmail.com>
2022-11-20 14:09:40 -0500

    Install dotenv module and create a const cors for using cors middleware inside index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const PORT = 8080;
 const videoList = require('./routes/video');
+const cors = require('cors');
 
+app.use(cors());
 app.use('/videos', videoList);
+
 
 app.listen(PORT, () => {
   console.log("Hello Beautiful!");
